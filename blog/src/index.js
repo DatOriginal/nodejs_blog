ba70bde6d9c8037fa2cd1af3eec3696fb4f22947
DatTT <original.dat1998@gmail.com>
2023-05-01 22:37:40 +0700

    [Nodejs] basic routing

--- blog/src/index.js
+++ blog/src/index.js
@@ -22,6 +22,9 @@ app.get('/', (req, res) => {
   res.render('home');
 })
 
+app.get('/news', (req, res) => {
+  res.render('news');
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 })
