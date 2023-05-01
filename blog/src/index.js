428f6b175eef56e1a86c4598038deda634410f0e
DatTT <original.dat1998@gmail.com>
2023-05-01 23:06:43 +0700

    [nodejs] basic query parameter

--- blog/src/index.js
+++ blog/src/index.js
@@ -20,11 +20,17 @@ app.set('views', path.join(__dirname,'resources/views'));
 
 app.get('/', (req, res) => {
   res.render('home');
-})
+});
 
 app.get('/news', (req, res) => {
   res.render('news');
-})
+});
+
+app.get('/search', (req, res) => {
+  console.log(req.query.q)
+  res.render('search');
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-})
+});
