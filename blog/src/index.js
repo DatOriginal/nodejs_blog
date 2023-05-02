0899dbfca3d7473a2c8b799cf3afeafc7b5ae2a3
DatTT <original.dat1998@gmail.com>
2023-05-02 21:40:25 +0700

    install prettier, lint-staged, husky

--- blog/src/index.js
+++ blog/src/index.js
@@ -1,4 +1,4 @@
-const path = require('path'); 
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars').engine;
@@ -9,25 +9,32 @@ const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 app.use(express.json());
 
 //HTTP Logger
 //app.use(morgan('combined'))
 
 //template engine
-app.engine('hbs', handlebars({
-  extname: '.hbs'
-}));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname,'resources/views'));
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+    }),
+);
+app.set(
+    'view engine',
+    'hbs',
+);
+                        app.set('views', path.join(__dirname, 'resources/views'));
 
 // Route init
-route(app);
+    route(app);
 
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+        app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`);
+        });
--- blog/src/routes/index.js
+++ blog/src/routes/index.js
@@ -1,9 +1,7 @@
 const newRouter = require('./news');
 const siteRouter = require('./site');
 
-
 function route(app) {
-
     app.use('/news', newRouter);
 
     app.use('/', siteRouter);
@@ -11,24 +9,22 @@ function route(app) {
     // app.get('/', (req, res) => {
     //     res.render('home');
     //   });
-      
+
     //   app.get('/news', (req, res) => {
     //     res.render('news');
     //   });
-      
+
     //   app.get('/search', (req, res) => {
     //     //console.log(req.query.q)
     //     res.render('search');
     //   });
-      
+
     //   app.post('/search', (req, res) => {
-      
+
     //     console.log(req.body);
-      
+
     //     res.send('');
     //   });
-      
-
 }
 
 module.exports = route;
