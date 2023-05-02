a2ddbbd0c17915339e8876453a03e0fbbdb75e1c
DatTT <original.dat1998@gmail.com>
2023-05-02 13:38:07 +0700

    POST method

--- blog/src/index.js
+++ blog/src/index.js
@@ -1,14 +1,20 @@
 const path = require('path'); 
 const express = require('express');
 const morgan = require('morgan');
+const { copyFileSync } = require('fs');
 const handlebars = require('express-handlebars').engine;
 const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
 //HTTP Logger
-app.use(morgan('combined'))
+//app.use(morgan('combined'))
 
 //template engine
 app.engine('hbs', handlebars({
@@ -29,6 +35,13 @@ app.get('/news', (req, res) => {
 app.get('/search', (req, res) => {
   console.log(req.query.q)
   res.render('search');
+});
+
+app.post('/search', (req, res) => {
+
+  console.log(req.body);
+
+  res.send('');
 });
 
 app.listen(port, () => {
